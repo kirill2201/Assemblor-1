c32545dd9827d97d75e7130d77abeae03a60fdeb
ZKugizakID <ziborov.kirill02@mail.ru>
2025-12-01 22:05:12 +0300

    C# added

---
+++ Assemblor1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Assemblor1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Assemblor-1 awakens...");
+        }
+    }
+}
